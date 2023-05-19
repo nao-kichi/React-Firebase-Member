3c8685fdbb023be286d1de2a04a900fcf31ed2a9
nao-kichi <namnam3225@gmail.com>
2023-05-20 02:12:38 +0900

    ファイル復元

--- src/FirebaseConfig.js
+++ src/FirebaseConfig.js
@@ -2,8 +2,6 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
-import { doc, getDoc } from "firebase/firestore";
-
 
 // 本番環境モード
 const firebaseConfig = {
